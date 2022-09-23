738a275b663e5f862484965ea78b676833a6f298
JesusTics <jesusantonio2000@hotmail.com>
2022-09-23 13:13:21 -0600

    Ya tiene los items de Recycler

--- app/src/main/java/com/aether/mymovieappmvp/MovieFavoriteFragment.kt
+++ app/src/main/java/com/aether/mymovieappmvp/MovieFavoriteFragment.kt
@@ -5,9 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.aether.mymovieappmvp.databinding.FragmentMovieFavoriteBinding
+import com.aether.mymovieappmvp.databinding.FragmentMovieMainBinding
 
 class MovieFavoriteFragment : Fragment(R.layout.fragment_movie_favorite) {
+
+    private lateinit var binding : FragmentMovieFavoriteBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieFavoriteBinding.bind(view)
     }
 }
--- app/src/main/java/com/aether/mymovieappmvp/MovieMainFragment.kt
+++ app/src/main/java/com/aether/mymovieappmvp/MovieMainFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.aether.mymovieappmvp.databinding.FragmentMovieMainBinding
 
 class MovieMainFragment : Fragment(R.layout.fragment_movie_main) {
 
+    private lateinit var binding : FragmentMovieMainBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieMainBinding.bind(view)
 
     }
 }
