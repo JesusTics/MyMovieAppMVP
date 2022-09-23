024a69a2f3c057be39c9735a9cc6754f85ef62da
JesusTics <jesusantonio2000@hotmail.com>
2022-09-23 11:23:11 -0600

    first commit

---
+++ app/src/main/java/com/aether/mymovieappmvp/MovieMainFragment.kt
@@ -0,0 +1,15 @@
+package com.aether.mymovieappmvp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MovieMainFragment : Fragment(R.layout.fragment_movie_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
+    }
+}
